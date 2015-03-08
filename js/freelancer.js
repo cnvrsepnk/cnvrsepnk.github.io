ba99e5af455e5a3524be391f07fbfe15d1b490ff
Vanessa Gill <vanny42@gmail.com>
2015-03-07 23:37:39 -0800

    `

--- js/freelancer.js
+++ js/freelancer.js
@@ -17,11 +17,11 @@ $(function() {
 
 $(function() {
     $(window).scroll(function(){
-        console.log('WINDOW: ' + $(this).scrollTop());
+        //console.log('WINDOW: ' + $(this).scrollTop());
         var port = Math.round($('#portfolio').offset().top);
-        console.log('PORT: ' + port);
+        //console.log('PORT: ' + port);
         if($(this).scrollTop() == port) {
-            console.log('yay!');
+            $('.navbar-brand').show();
         }
     });
     
