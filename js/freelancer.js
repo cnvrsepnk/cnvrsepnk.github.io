6df6d0abf1f5a8a64f2e252ff3c3c55423e932c2
Vanessa Gill <vanny42@gmail.com>
2015-03-08 21:02:39 -0700

    `

--- js/freelancer.js
+++ js/freelancer.js
@@ -36,6 +36,9 @@ $(function() {
         if($(window).width() > 768) {
             about_col_1.addClass('wow slideInLeft');
             about_col_2.addClass('wow slideInRight');
+        } else {
+            about_col_1.addClass('wow fadeInUp');
+            about_col_2.addClass('wow fadeInUp');
         }
     });
     
